e0110e96e577a042c9c249c12a13a02375b8d3b9
majomarquez <majomarquez@gmail.com>
2019-07-01 08:14:36 -0400

    promesa lee la informacion del md pero no funciona markdownLinkExtractor

--- src/md-links.js
+++ src/md-links.js
@@ -8,19 +8,35 @@ const chalk = require('chalk')
 // const read = fs.readFileSync ("./Readme.md")// version sincrona de leer un archivo por su ruta
 // console.log(read)
 
-  const read = fs.readFile("./Readme.md", "utf-8",(error,datos) => { // se agrega la funcion con 3 parametros , el primero es de error y el segundo la forma en que vere el archivo y el tercero si hay un error
+// declaracion de la promesa
+const read = (fileName, type)=>{   //tomo la funcion declarada que tiene dos parametros 
+  return new Promise ((resolve,reject)=>{       //   le digo quelafuncion va a retornar una promesa
+    fs.readFile(fileName, type ,(error, content) =>{   //llamo a la funcion de filesystem
+
     if (error) {
       reject(error)
     }
     else {
-      console.log("En el Archivo solicitado se encuentran los siguientes Links:") 
+      resolve(content) ;
     }
-    })
+  });
+});
+}
 
-  const markdown = fs.readFileSync('README.md').toString();
-  const links = markdownLinkExtractor(markdown);
-  links.forEach(function (infoLinks) {
-    fetch(infoLinks)
+ // llamada de la promesa
+let paths= process.argv[2]
+console.log( paths);
+ read(paths, "utf-8")
+  .then(res => {
+    console.log(res)
+  })
+  .catch (err=>{
+    console.log (err);
+  })
+
+  var links = markdownLinkExtractor(read);
+  links.forEach(function (link) {
+    fetch(link)
       .then((res) => {
         if (res.ok){
           console.log (chalk.magenta("This Link is Working status : ") + chalk.cyan (res.status + " " + res.url) )
@@ -31,7 +47,18 @@ const chalk = require('chalk')
       })
 })
 
+// Promise.all([read(path, "utf-8")]) //le paso cada una de las declaaraciones de promesas separadas por coma y deuelve una promesa
+// .then(res=>{
+//   console.log(res[1]);// solome da la respuesta de la promesa numero 1 en este caso readfile
+// })
+// .catch (err=>{
+//   console.log (err);
+// })
 
-// exports.readAFile= readAFile;
-// exports.readFilesAndExtractLinks=readFilesAndExtractLinks;
+  
+
+
+
+// // exports.readAFile= readAFile;
+// // exports.readFilesAndExtractLinks=readFilesAndExtractLinks;
 
